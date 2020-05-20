264ae3e0d88a15247d3a1039df7a28b961776cc4
Kilo Loco <kiloloco@me.com>
2020-05-19 19:12:02 -0700

    Initial Commit

---
+++ Just Start Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Just Start Testing
+//
+//  Created by Kyle Lee on 5/19/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
